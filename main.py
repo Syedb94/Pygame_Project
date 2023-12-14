015faed8fff033bea3ae4864dc6624b61766c2b0
syedb94 <syedb94@kprschools.ca>
2023-12-14 09:30:39 -0500

    test git

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 import random
 pygame.font.init()
 
+#test git
+
 WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Shooter Tutorial")
